b5b21cffac5c740f93d4c274f2f4f3177da33f5b
Sidhu Achary <siddhuachary2005@gmail.com>
2024-11-26 18:50:12 +0530

    utils dir dicussion

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,14 +1,20 @@
-// const asyncHandler = () => {};
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+        .catch((err)=> console.log(err))
+    } 
+} 
+
+
+// const asyncHandler = () => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
 
 export default asyncHandler;
-
-const asyncHandler = () => async (req, res, next) => {
-    try {
-        await fn(req, res, next)
-    } catch (error) {
-        res.status(error.code || 500).json({
-            success: false,
-            message: error.message
-        })
-    }
-}
